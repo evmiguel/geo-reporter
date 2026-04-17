dedf4f5ad808a2b3462dfe9dfe68cba5231c3ceb
Erika Miguel <erika@erikamiguel.com>
2026-04-17 17:38:38 -0400

    feat(v3): add unit vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['tests/unit/**/*.test.ts'],
+    exclude: ['tests/integration/**'],
+    testTimeout: 5_000,
+  },
+})
