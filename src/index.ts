177d1c6be8fa85f920478ee44b7e2421a5f425b4
Erika Miguel <erika@erikamiguel.com>
2026-04-17 17:39:14 -0400

    feat(v3): expose package surface via src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export {}
+export { env, loadEnv } from './config/env.ts'
+export { db, type Db } from './db/client.ts'
+export * as schema from './db/schema.ts'
+export * from './store/types.ts'
+export { PostgresStore } from './store/postgres.ts'
+export { createRedis } from './queue/redis.ts'
+export {
+  enqueueGrade,
+  enqueueReport,
+  enqueuePdf,
+  getGradeQueue,
+  getReportQueue,
+  getPdfQueue,
+  gradeQueueName,
+  reportQueueName,
+  pdfQueueName,
+  type GradeJob,
+  type ReportJob,
+  type PdfJob,
+} from './queue/queues.ts'
