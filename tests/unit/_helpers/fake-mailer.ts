d501508db71d1cd70ee0990969c5e209d224f357
Erika Miguel <erika@erikamiguel.com>
2026-04-20 22:08:39 -0400

    feat(mail): Mailer.sendContactMessage for contact-form forwarding

--- tests/unit/_helpers/fake-mailer.ts
+++ tests/unit/_helpers/fake-mailer.ts
@@ -1,8 +1,9 @@
-import type { Mailer, MagicLinkMessage, RefundNoticeMessage } from '../../../src/mail/types.ts'
+import type { Mailer, MagicLinkMessage, RefundNoticeMessage, ContactMessage } from '../../../src/mail/types.ts'
 
 export class FakeMailer implements Mailer {
   sent: MagicLinkMessage[] = []
   public readonly refundNotices: RefundNoticeMessage[] = []
+  public readonly contactMessages: ContactMessage[] = []
 
   async sendMagicLink(msg: MagicLinkMessage): Promise<void> {
     this.sent.push(msg)
@@ -12,8 +13,13 @@ export class FakeMailer implements Mailer {
     this.refundNotices.push(msg)
   }
 
+  async sendContactMessage(msg: ContactMessage): Promise<void> {
+    this.contactMessages.push(msg)
+  }
+
   reset(): void {
     this.sent = []
     this.refundNotices.length = 0
+    this.contactMessages.length = 0
   }
 }
